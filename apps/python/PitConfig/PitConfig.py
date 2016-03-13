9ed0b1db9afbb420e4f3de799e935014bb537c17
Marocco2 <marocco2@live.it>
2016-03-13 11:07:19 +0100

    1.3
    - AutoUpdate feature is under PitConfig.py
    - CheckNewVersion.exe is now useless

--- apps/python/PitConfig/PitConfig.py
+++ apps/python/PitConfig/PitConfig.py
@@ -21,6 +21,7 @@ import subprocess
 import configparser
 #from requests import get
 import urllib.request
+import json
 import shutil
 import codecs
 import platform
@@ -35,12 +36,20 @@ import ctypes
 from PitConfig_lib import sim_info
 from PitConfig_lib import pahk
 
-version = "1.2.1"
+version = "1.3"
 #r = get('https://api.github.com/repos/Marocco2/PitConfig-Marocco2-version/releases/latest')
 #request = r.json()
 #lastversion = request['tag_name']
 #if version != lastversion:
-urllib.request.urlretrieve("https://api.github.com/repos/Marocco2/PitConfig-Marocco2-version/releases/latest", "version.json")
+request = urllib.request.urlopen("https://api.github.com/repos/Marocco2/PitConfig-Marocco2-plugin/releases/latest")
+tag_name = json.load(request)
+lastversion = tag_name['tag_name']
+OTAini = configparser.ConfigParser()
+OTAini.read('apps\python\PitConfig\PitConfig.ini')
+OTA = OTAini['AUTOUPDATE']['Enable']
+if version != lastversion and OTA == 1:
+    urllib.request.urlrequest("https://raw.githubusercontent.com/Marocco2/PitConfig-Marocco2-plugin/"+str(lastversion)+"/apps/python/PitConfig/PitConfig.py", "PitConfig.py")
+
 
 user32 = ctypes.windll.user32
 Resolution = user32.GetSystemMetrics(0)
